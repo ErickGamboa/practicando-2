61ab600b6031d8a09086b4db1c8d2340c4067ce9
erick gamboa <erick.yosue@gmail.com>
2020-10-01 17:05:27 -0600

    primer cambio

---
+++ practicando 2/practicando 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicando_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = 1;
+        }
+    }
+}
